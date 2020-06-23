a5f6d0b2b448c543ac7170e4ae0dbf87b8fab993
harshvardhanpatil <007harsh007patil@gmail.com>
2020-06-23 12:43:10 +0530

    Structured Folders And Login is fixed

---
+++ app/src/main/java/com/sih2020/abhyuday/Hospital/HospitalHomeScreenActivity.java
@@ -0,0 +1,16 @@
+package com.sih2020.abhyuday.Hospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sih2020.abhyuday.R;
+
+public class HospitalHomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_home_screen);
+    }
+}
