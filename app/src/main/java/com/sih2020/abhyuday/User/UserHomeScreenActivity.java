fc9a9fc1db70ec394cca93765f2b675142cc9494
harshvardhanpatil <007harsh007patil@gmail.com>
2020-06-23 12:38:44 +0530

    Structured Folders And Login is fixed

---
+++ app/src/main/java/com/sih2020/abhyuday/User/UserHomeScreenActivity.java
@@ -0,0 +1,16 @@
+package com.sih2020.abhyuday.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sih2020.abhyuday.R;
+
+public class UserHomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home_screen);
+    }
+}
