6f81bebd86e4d6358e1241395c38b631afba6963
Durgnan <durgnan45@gmail.com>
2020-08-01 01:41:58 +0530

    TrackerMapsActivity Completed 90%

--- app/src/main/java/com/sih2020/abhyuday/Tracking/TrackerMapsActivity.java
+++ app/src/main/java/com/sih2020/abhyuday/Tracking/TrackerMapsActivity.java
@@ -1,26 +1,50 @@
 package com.sih2020.abhyuday.Tracking;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.NumberPicker;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
 import com.sih2020.abhyuday.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrackerMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -33,6 +57,8 @@ public class TrackerMapsActivity extends FragmentActivity implements OnMapReadyC
     private Polyline polyline;
     private PolylineOptions polylineOptions;
     private LocationManager locationManager;
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,11 +69,23 @@ public class TrackerMapsActivity extends FragmentActivity implements OnMapReadyC
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //Firebase Instantiation
+        database = FirebaseDatabase.getInstance();
+
+
+
+        source = new LatLng(0,0 );
+        destination = new LatLng(0,0 );
+
+
         bundle = getIntent().getExtras();
         source = new LatLng(Double.parseDouble(bundle.getString("source").split(",")[0]), Double.parseDouble(bundle.getString("source").split(",")[1]));
         destination = new LatLng(Double.parseDouble(bundle.getString("destination").split(",")[0]), Double.parseDouble(bundle.getString("destination").split(",")[1]));
         transactionID = bundle.getString("transactionID");
         userType = bundle.getString("userType");
+        reference = database.getReference("Trips").child(transactionID);
+
+
     }
 
     /**
@@ -63,14 +101,16 @@ public class TrackerMapsActivity extends FragmentActivity implements OnMapReadyC
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(12.966784, 77.673830),(float)14.0));
 
         if(userType.equalsIgnoreCase("ambulance"))
 
         {
+            if(destinationM != null)
+                destinationM.remove();
+
+            destinationM = mMap.addMarker(new MarkerOptions().position(destination).title("Patient").icon(BitmapDescriptorFactory.fromResource(R.drawable.patient)));
             locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -87,12 +127,43 @@ public class TrackerMapsActivity extends FragmentActivity implements OnMapReadyC
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,5000,5,locationListener);
 
         }
+        else
+        {
+            if(source != null)
+            {
+                sourceM.remove();
+            }
+            sourceM = mMap.addMarker(new MarkerOptions().title("Me").position(source).icon(BitmapDescriptorFactory.fromResource(R.drawable.patient)));
+
+            reference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    destination = dataSnapshot.child("destination").getValue(LatLng.class);
+                    if(destinationM != null)
+                        destinationM.remove();
+                    destinationM = mMap.addMarker(new MarkerOptions().position(destination).title("Ambulance").icon(BitmapDescriptorFactory.fromResource(R.drawable.ambulance)));
+                    getDirections(source,destination);
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+
+        }
+
     }
 
     LocationListener locationListener = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
-
+            source = new LatLng(location.getLatitude(),location.getLongitude());
+            pushLocationToFirebase(source,transactionID);
+            updateMarkers(source);
+            getDirections(source,destination);
 
         }
 
@@ -137,4 +208,101 @@ public class TrackerMapsActivity extends FragmentActivity implements OnMapReadyC
         super.onBackPressed();
         finish();
     }
+
+
+    private void getDirections(LatLng source, LatLng destination)
+    {
+        if(polyline != null)
+            polyline.remove();
+
+
+        List<LatLng> paths = new ArrayList<>();
+
+        GeoApiContext context  = new GeoApiContext.Builder().apiKey(getResources().getString(R.string.google_maps_key)).build();
+        DirectionsApiRequest req = DirectionsApi.getDirections(context,source.latitude+","+source.longitude,destination.latitude+","+destination.longitude);
+        try {
+            DirectionsResult res = req.await();
+
+            //Loop through legs and steps to get encoded polylines of each step
+            if (res.routes != null && res.routes.length > 0) {
+                DirectionsRoute route = res.routes[0];
+
+                if (route.legs !=null) {
+                    for(int i=0; i<route.legs.length; i++) {
+                        DirectionsLeg leg = route.legs[i];
+                        if (leg.steps != null) {
+                            for (int j=0; j<leg.steps.length;j++){
+                                DirectionsStep step = leg.steps[j];
+                                if (step.steps != null && step.steps.length >0) {
+                                    for (int k=0; k<step.steps.length;k++){
+                                        DirectionsStep step1 = step.steps[k];
+                                        EncodedPolyline points1 = step1.polyline;
+                                        if (points1 != null) {
+                                            //Decode polyline and add points to list of route coordinates
+                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                            for (com.google.maps.model.LatLng coord1 : coords1) {
+                                                paths.add(new LatLng(coord1.lat, coord1.lng));
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    EncodedPolyline points = step.polyline;
+                                    if (points != null) {
+                                        //Decode polyline and add points to list of route coordinates
+                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                        for (com.google.maps.model.LatLng coord : coords) {
+                                            paths.add(new LatLng(coord.lat, coord.lng));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch(Exception ex) {
+            Log.e("TAG", ex.getLocalizedMessage());
+        }
+
+        //Draw the retrieved polylines
+        if(paths.size() > 0)
+        {
+            polylineOptions = new PolylineOptions().addAll(paths).color(Color.GREEN).width(5);
+            polyline = mMap.addPolyline(polylineOptions);
+        }
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        //Centre Animate the Map
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(source);
+        builder.include(destination);
+        LatLngBounds bounds = builder.build();
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds,200);
+        mMap.animateCamera(cu);
+
+
+        return;
+
+
+
+    }
+
+    private void pushLocationToFirebase(LatLng location, String transactionID)
+    {
+        //Firebase code
+        reference.child("destination").setValue(location);
+    }
+
+    //TODO: Only For Updating Ambulance's Marker when Ambulance is Using this
+    private void updateMarkers(LatLng location)
+    {
+            if(sourceM != null)
+                sourceM.remove();
+            sourceM = mMap.addMarker(new MarkerOptions().position(location).title("Me").icon(BitmapDescriptorFactory.fromResource(R.drawable.ambulance)));
+
+    }
+
+
+
+
 }
